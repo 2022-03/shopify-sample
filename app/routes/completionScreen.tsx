9fae7e2bd25bdb4857e9eab4c4e3b07254246318
2022-03 <shiki10masaoka14+netlify01@gmail.com>
2022-05-10 13:07:45 +0000

    CompletionScreen

--- app/routes/completionScreen.tsx
+++ app/routes/completionScreen.tsx
@@ -57,8 +57,12 @@ const CompletionScreen: VFC = () => {
 
   return (
     <Layout quantity={quantity} isLoading={isLoading}>
-      <h1>お問い合わせ内容を送信しました</h1>
-      <Link to={`/`}>ホームへ</Link>
+      <div className="flex h-[calc(100vh_-_152px)] flex-col justify-center space-y-10 pb-[36px] text-center md:h-[calc(100vh_-_124px)]">
+        <h1 className="text-xl sm:text-2xl">
+          お問い合わせ内容を送信しました
+        </h1>
+        <Link to={`/`}>ホームへ戻る</Link>
+      </div>
     </Layout>
   );
 };
